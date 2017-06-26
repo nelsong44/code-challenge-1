dc86b2f6fd7fad8051c9e61b468732ff3e7db814
nelsong44 <nelsong44@gmail.com>
2017-06-26 14:22:45 -0500

    everything working except counter. It's count is the same for each div appended to DOM

--- client.js
+++ client.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
   console.log('JQ sourced');
   $('button').on('click', function() {
-    counter ++;
     append();
   });// end click
   $('.output').on('click', '.swap', swapColors);
   $('.output').on('click', '.delete', deleteParent);
 }); // end onReady
 
-// var attach =
-
 //counter
 var counter = 0;
 
+var attach = '<div class="main"><p>' + counter + '</p></div>';
+// var secondAttach = '<button class="swap">Swap</button>' +
+//                   '<button class="delete">Delete</button>';
+
+
 //append to DOM
 function append() {
-  $('.output').append('<div class="main"></div>');
-  $('.main').append('<p>' + counter + '</p>');
+  counter ++;
+  $('.output').append(attach);
+  $('.main').html('<p>' + counter + '</p>');
   $('.main').append('<button class="swap">Swap</button>' +
                     '<button class="delete">Delete</button>');
   console.log(counter);
@@ -30,5 +33,9 @@ function swapColors() {
 
 //delete parent div of the 'delete' button that is clicked
 function deleteParent() {
+  console.log($(this).parent());
+  console.log($(this).children());
+  console.log($(this).siblings());
+  console.log($(this));
   $(this).parent().remove();
 }// end deleteParent
